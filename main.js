46b9ae5131b1801c21ad91f82375f8dcce9f8489
Ojo Farouq Abiodun <harbyodunojo@gmail.com>
2025-08-31 07:50:09 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,52 +1,26 @@
-const showMenu = document.querySelector(".showMenu");
-const closeMenu = document.querySelector(".closeMenu");
-const mobileMenu = document.querySelector(".mobile");
-const overlay = document.querySelector(".overlay");
+const faqs = document.querySelectorAll(".q");
 
-function openMenu() {
-	mobileMenu.classList.add("active");
-	overlay.classList.add("active");
-}
-
-function closeMenuFunc() {
-	mobileMenu.classList.remove("active");
-	overlay.classList.remove("active");
-}
-
-showMenu.addEventListener("click", openMenu);
-
-closeMenu.addEventListener("click", closeMenuFunc);
-
-overlay.addEventListener("click", closeMenuFunc);
-
-document.addEventListener("keydown", (e) => {
-	if (e.key === "Escape") {
-		closeMenuFunc();
-	}
+faqs.forEach((q) => {
+	q.addEventListener("click", () => {
+		if (q.classList.contains("active")) {
+			q.classList.remove("active");
+		} else {
+			q.classList.add("active");
+            faqs.forEach((otherq)=>{
+                if(otherq != q){
+                    otherq.classList.remove("active")
+                }
+            })
+		}
+	});
 });
 
-const featureToggle = document.querySelector("#feature > a");
-const companyToggle = document.querySelector("#company > a");
-const mobileFeatures = document.querySelector(".mobile-features-links");
-const mobileCompany = document.querySelector(".mobile-company-links");
-const featureArrow = document.querySelector("#feature img");
-const companyArrow = document.querySelector("#company img");
+let nav = document.querySelector(".nav");
 
-function toggleDropdown(dropdown, arrow) {
-	dropdown.classList.toggle("show-dropdown");
-	if (dropdown.classList.contains("show-dropdown")) {
-		arrow.src = "./images/icon-arrow-up.svg";
-	} else {
-		arrow.src = "./images/icon-arrow-down.svg";
-	}
+function showMenu() {
+    nav.classList.add("show");
 }
 
-featureToggle.addEventListener("click", (e) => {
-	e.preventDefault(); // Prevent link navigation
-	toggleDropdown(mobileFeatures, featureArrow);
-});
-
-companyToggle.addEventListener("click", (e) => {
-	e.preventDefault();
-	toggleDropdown(mobileCompany, companyArrow);
-});
+function hideMenu() {
+    nav.classList.remove("show");
+}
